4d0da06ed8d738124d0d5d01252445e5d607ad1b
Fraz Ahsan <fraz.ahsan17@gmail.com>
2017-04-03 18:50:09 +0500

    Action cable url added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://gradient-test.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://gradient-test.herokuapp.com', 'http://gradient-test.herokuapp.com']
 end
